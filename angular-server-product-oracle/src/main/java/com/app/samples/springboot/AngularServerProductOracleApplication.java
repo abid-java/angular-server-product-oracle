8da468368999c1fafc8c5c3b503cd1f9648516d9
abmohamm <mohd.92java@gmail.com>
2021-01-15 21:16:53 -0500

    [US1234]:Spring-Boot Skeleton Application

---
+++ angular-server-product-oracle/src/main/java/com/app/samples/springboot/AngularServerProductOracleApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularServerProductOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularServerProductOracleApplication.class, args);
+	}
+
+}
